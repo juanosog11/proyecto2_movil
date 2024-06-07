bfbf8e5f2bfded67c5841e9d96e0496fae077734
owen holguin <andresholguin1706@gmail.com>
2024-06-07 01:42:39 -0500

    Se realizaron Cambios en la interfaz de perfil

--- src/Interfaces/Perfil.jsx
+++ src/Interfaces/Perfil.jsx
@@ -1,99 +1,83 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Image, Text, TouchableOpacity, View, ScrollView } from 'react-native';
-import { estilosPerfil, estilos_estandar } from "./Estilos.jsx";
+import { estilosPerfil, estilos_estandar } from './Estilos.jsx';
 import NavBar from '../components/Navbar.jsx';
 
 export default function Perfil({ navigation, route }) {
-
-  const [imagen,setImagen] = useState('');
-  const [nombre,setNombre] = useState('');
-  const [pais,setPaises] = useState('');
+  const [imagen, setImagen] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [pais, setPaises] = useState('');
   const [acciones, setAcciones] = useState('');
-  const [saldo,setSaldo] = useState('');
-  const [correo,setCorreo] = useState('');
+  const [saldo, setSaldo] = useState('');
+  const [correo, setCorreo] = useState('');
   const [numero, setNumero] = useState('');
 
-  const handlePerfil = async () =>{
+  const datosU = route.params.usuario;
+  console.log(datosU);
+  const usuario = datosU;
+  console.log('acciones usuario', usuario.id);
+
+  const PaisB = async () => {
     try{
-    console.log(usuario)
-    const MostrarUsuario = await fetch('http://localhost:3001/Usuario',{
-    method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-        },
-        body: JSON.stringify(usuario),
-        });
-
-        const dataU = await MostrarUsuario.json();
-        setImagen(dataU.imagen);
-        setNombre(dataU.nombre);
-        setPaises(dataU.pais);
-        setAcciones(dataU.acciones);
-        setSaldo(dataU.saldo);
-        setCorreo(dataU.correo);
-        setNumero(dataU.numero);
-
-        console.log(dataU)
-
+      const BuscarPais = await fetch(`http://localhost:3001/Pais/${usuario.pais_id}`);
+    const dataPais = await BuscarPais.json();
+    const nomPais = dataPais.nombre;
+    console.log('nombre del país:', nomPais);
+    setPaises(nomPais);
     }catch(error){
 
     }
-    console.log(dataU);
-  }
+  };
 
   useEffect(() => {
-    handlePerfil();
+    PaisB(); // Llamar a la función cuando se monte el componente
   }, []);
 
-  const { usuario } = route.params;
-  console.log('Perfil:', JSON.stringify(usuario));
-
-    return (
-      <ScrollView>
-        <View style={estilos_estandar.container}>
-          <View style={estilos_estandar.head}>
-            <Text style={estilos_estandar.title}>smartInvest</Text>
-            <Image source={require('../images/logo.jpeg')} style={estilos_estandar.logo} />
-          </View>
-          <View style={estilosPerfil.form}>
-            <Image source={require('../images/images (2).jpeg')} style={estilosPerfil.logo} />
-            <Text style={estilosPerfil.title}>Datos del Usuario</Text>
-            <View style={estilosPerfil.userData}>
-              <Text style={estilosPerfil.userDataLabel}>Nombre y apellidos:</Text>
-              <Text>{nombre}</Text>
-            </View>
-            <View style={estilosPerfil.userData}>
-              <Text style={estilosPerfil.userDataLabel}>País:</Text>
-              <Text>{pais}</Text>
-            </View>
-            <View style={estilosPerfil.userData}>
-              <Text style={estilosPerfil.userDataLabel}>Acciones:</Text>
-              <Text>{acciones}</Text>
-            </View>
-            <View style={estilosPerfil.userData}>
-              <Text style={estilosPerfil.userDataLabel}>Saldo:</Text>
-              <Text>${saldo}</Text>
-            </View>
-            <View style={estilosPerfil.userData}>
-              <Text style={estilosPerfil.userDataLabel}>Correo:</Text>
-              <Text>{correo}</Text>
-            </View>
-            <View style={estilosPerfil.userData}>
-              <Text style={estilosPerfil.userDataLabel}>Número:</Text>
-              <Text>+{numero}</Text>
-            </View>
-
-            <View style={estilosPerfil.buttonContainer}>
-              <Button
-                title='Modificar Informacion'
-                onPress={() => navigation.navigate('Modificar',{usuario})} style={estilosPerfil.button}
-              ></Button>
-            </View>
-          </View>
-          <NavBar navigation={navigation} usuario={usuario} />
+  return (
+    <ScrollView>
+      <View style={estilos_estandar.container}>
+        <View style={estilos_estandar.head}>
+          <Text style={estilos_estandar.title}>smartInvest</Text>
+          <Image source={require('../images/logo.jpeg')} style={estilos_estandar.logo} />
         </View>
-      </ScrollView>
-    );
+        <View style={estilosPerfil.form}>
+          <Image source={require('../images/images (2).jpeg')} style={estilosPerfil.logo} />
+          <Text style={estilosPerfil.title}>Datos del Usuario</Text>
+          <View style={estilosPerfil.userData}>
+            <Text style={estilosPerfil.userDataLabel}>Nombre y apellidos:</Text>
+            <Text>{usuario.nombre}</Text>
+          </View>
+          <View style={estilosPerfil.userData}>
+            <Text style={estilosPerfil.userDataLabel}>País:</Text>
+            <Text>{pais}</Text>
+          </View>
+          <View style={estilosPerfil.userData}>
+            <Text style={estilosPerfil.userDataLabel}>Acciones:</Text>
+            <Text>{usuario.acciones}</Text>
+          </View>
+          <View style={estilosPerfil.userData}>
+            <Text style={estilosPerfil.userDataLabel}>Saldo:</Text>
+            <Text>${usuario.saldo}</Text>
+          </View>
+          <View style={estilosPerfil.userData}>
+            <Text style={estilosPerfil.userDataLabel}>Correo:</Text>
+            <Text>{usuario.correo}</Text>
+          </View>
+          <View style={estilosPerfil.userData}>
+            <Text style={estilosPerfil.userDataLabel}>Número:</Text>
+            <Text>+{usuario.numero}</Text>
+          </View>
+
+          <View style={estilosPerfil.buttonContainer}>
+            <Button
+              title='Modificar Informacion'
+              onPress={() => navigation.navigate('Modificar', { usuario })}
+              style={estilosPerfil.button}
+            ></Button>
+          </View>
+        </View>
+        <NavBar navigation={navigation} usuario={usuario} />
+      </View>
+    </ScrollView>
+  );
 }
